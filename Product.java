9f131f4babe367ddf3a1ba787d9459eea402f031
AndyPendragon <116746292+AndyPendragon@users.noreply.github.com>
2024-05-07 22:32:01 +0300

    refactor: use enum for productName

--- Product.java
+++ Product.java
@@ -2,9 +2,11 @@ import java.time.Instant;
 
 public class Product{
   String Id;
-  String name;
+  ProductName productName;
   double quantity;
   double unitPricePerLiter;
   Integer evaporationRate;
   Instant lastSupplyDate;
 }
+
+enum ProductName {PETROLEUM, GAS, DIESEL}
