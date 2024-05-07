832aa32c360839ef6ef2dc28f274a0d9102757ee
AndyPendragon <116746292+AndyPendragon@users.noreply.github.com>
2024-05-07 20:49:45 +0300

    feat: add ServiceStation class

---
+++ ServiceStation.java
@@ -0,0 +1,10 @@
+import java.util.List;
+import java.time.Instant;
+
+public class ServiceStation {
+  String id;
+  String name;
+  List<Product> products;
+  Instant lastUpdate;
+  String Location;
+}
